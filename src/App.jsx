dc5d1627b7fc38bc5c77f544ead30179971a991e
Worrick <worrick1717@gmail.com>
2024-02-21 19:07:30 +0545

    all components of agency profile page is created and template is desgined with reuseability approach

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import Header from "./components/Header";
 import LandingTemplate from "./templates/LandingTemplate";
 import Dashboard from "./templates/DashboardTemplate";
 import AdminPackageAdd from "./templates/AdminPackageAdd";
+import AgencyProfileAdd from "./templates/AgencyProfileAdd";
 
 function App() {
     return (
         <>
             {/* <Header /> */}
             {LandingTemplate}
-            {/*<Dashboard />*/}
-            <AdminPackageAdd />
+            {/* <Dashboard /> */}
+            {/* <AdminPackageAdd /> */}
+            <AgencyProfileAdd/>
         </>
     );
 }
