455c7b09b34983a01e25d290850970682fcff8be
seejn <seejnmaharjan@gmail.com>
2024-02-22 21:10:21 +0545

    added routes for templates
    created routes for templates
    - path link in header's anchor link

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
-import Header from "./components/Header";
-import LandingTemplate from "./templates/LandingTemplate";
-import Dashboard from "./templates/DashboardTemplate";
-import AdminPackageAdd from "./templates/AdminPackageAdd";
-import OrderPackage from "./templates/OrderPackage";
-import AgencyProfileAdd from "./templates/AgencyProfileAdd";
+import Router from "./router/Router.jsx";
 
 function App() {
     return (
         <>
-            {/* <Header /> */}
-            {LandingTemplate}
-            {/* <Dashboard /> */}
-            {/* <AdminPackageAdd /> */}
-            <AgencyProfileAdd/>
-            <OrderPackage/>
+            <Router />
         </>
     );
 }
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,17 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingTemplate from "../templates/LandingTemplate";
 import DashboardTemplate from "../templates/DashboardTemplate";
+import AdminPackageAdd from "../templates/AdminPackageAdd";
+import AgencyProfile from "../templates/AgencyProfile";
+import OrderPackage from "../templates/OrderPackage";
 
-const Router = () =>{
- return(
-    <>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<LandingTemplate/>}/>
-                <Route path="/dashboard" element={<DashboardTemplate/>}/>
-            </Routes>
-        </BrowserRouter>
-    </>
- );
-}
-export default Router
+const Router = () => {
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<LandingTemplate />} />
+                    <Route path='/dashboard' element={<DashboardTemplate />} />
+                    <Route path='/agency-profile' element={<AgencyProfile />} />
+                    <Route
+                        path='/admin-package-add'
+                        element={<AdminPackageAdd />}
+                    />
+                    <Route path='/order-package' element={<OrderPackage />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
+};
+export default Router;
