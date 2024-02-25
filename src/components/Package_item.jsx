ef421d634fadf3cb0f6dc2fe4f5fcc080f4b1f9b
seejn <seejnmaharjan@gmail.com>
2024-02-25 10:07:45 +0545

    display package cards based on redux state
    import { useSelector }
    - const allPackages = useSelector(state => state.package)
    - allPackages.map((item, i) => <Item key={i} props={item}/>);

--- src/components/Package_item.jsx
+++ src/components/Package_item.jsx
@@ -1,17 +1,76 @@
-import React from 'react'
-import all_packages from '../assets/all_packages.js'
-import Item from './Item.jsx';
+import React from "react";
+// import all_packages from "../assets/all_packages.js";
+import { useSelector } from 'react-redux'
+
+export const Item = ({ props }) => {
+    console.log("Item: ", props);
+    const { packageName, imageUrl, price } = props;
+    return (
+        <div className='bg-white rounded-lg shadow-md p-6'>
+            <a href='#' className='text-lg font-semibold text-gray-900 mb-4 block'>
+                {packageName}
+            </a>
+            <a href='#'>
+                <img src={`http://localhost/assets/${imageUrl}.png`} alt='Travel Agency' className='mb-4 rounded-lg' />
+            </a>
+            <div className='grid grid-cols-2 gap-2'>
+                <div className='col-span-2'>
+                    <p className='text-gray-700'>
+                        <strong>Package Name:</strong> Package 2
+                    </p>
+                </div>
+                <div className='col-span-2'>
+                    <p className='text-gray-700'>
+                        <strong>Destination:</strong> Destination Name
+                    </p>
+                </div>
+                <div className='col-span-2'>
+                    <p className='text-gray-700'>
+                        <strong>Description:</strong> Description of Package 2
+                        goes here.
+                    </p>
+                </div>
+                <div>
+                    <p className='text-gray-700'>
+                        <strong>Start Date:</strong> January 1, 2024
+                    </p>
+                </div>
+                <div>
+                    <p className='text-gray-700'>
+                        <strong>End Date:</strong> January 7, 2024
+                    </p>
+                </div>
+                <div>
+                    <p className='text-gray-700'>
+                        <strong>Duration:</strong> 7 days
+                    </p>
+                </div>
+                <div className='col-span-2 mt-2'>
+                    <p className='text-gray-700'>
+                        <strong>Price:</strong> Rs {price}
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 const Package_item = () => {
-  return (
-    <div class="container mx-auto mt-8 px-4">
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
-          {all_packages.map((item, i)=>{
-              return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
-          })}
+    const allPackages = useSelector(state => state.package)
+    return (
+        <div className='container mx-auto mt-8 px-4'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4'>
+                {allPackages.map((item, i) => {
+                    return (
+                        <Item
+                            key={i}
+                            props={item}
+                        />
+                    );
+                })}
+            </div>
         </div>
-    </div>
-  )
-}
+    );
+};
 
-export default Package_item
+export default Package_item;
