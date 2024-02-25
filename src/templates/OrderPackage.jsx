36f5c3969f97e031c6ab175ab82e118eb962ec1c
seejn <seejnmaharjan@gmail.com>
2024-02-25 19:14:16 +0545

    package details displayed: route packageId
    get packageId from param, match with redux package state then display the details of matched package

--- src/templates/OrderPackage.jsx
+++ src/templates/OrderPackage.jsx
@@ -1,17 +1,25 @@
 import React from "react";
+import { useParams } from "react-router-dom"
 import { Item } from "../components/Package_item";
 import BookingDetails from "../components/BookingDetails";
 import PageLayout from "../components/PageLayout";
 import all_packages from "../assets/all_packages";
+import { useSelector } from "react-redux"
 
 const OrderPackage = () => {
-    const item = all_packages[0];
+    const { packageId } = useParams();
+    const packageItem = useSelector(state => state.package)
+    const item = packageItem.find(item => item.id == packageId)
+
+    // console.log("Product Ordered ID: ", packageId)
+    console.log("Item: ", item)
+
     return (
         <PageLayout>
             <div className='container mx-auto mt-8 px-4'>
                 <div className='bg-white rounded-lg shadow-md p-6 grid grid-cols-1 md:grid-cols-2 gap-8'>
                     <div className='bg-gray-100 rounded-lg shadow-md p-6'>
-                        <Item props={item} />
+                        <Item props={item} showButton={false}/>
                     </div>
                     <div className='bg-gray-100 rounded-lg shadow-md p-6'>
                         <BookingDetails />
