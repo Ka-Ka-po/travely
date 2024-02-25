a39f420589672c4710a9078c2b5ea3bb4a307db9
seejn <seejnmaharjan@gmail.com>
2024-02-25 10:11:10 +0545

    cleanup
    unnecessary files deleted
    - deleted: Item.js
    - deleted: PackageDetails.jsx
    - modified: typos in Header.jsx [ class -> className ]

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,20 @@
 function Header() {
     return (
         <>
-            <header class='bg-gray-900 text-white py-4'>
-                <h1 class='text-center'>Destination Travel Packages</h1>
+            <header className='bg-gray-900 text-white py-4'>
+                <h1 className='text-center'>Destination Travel Packages</h1>
             </header>
-            <nav class='bg-gray-800 text-white py-2'>
-                <a href='/dashboard' class='mx-2'>
+            <nav className='bg-gray-800 text-white py-2'>
+                <a href='/dashboard' className='mx-2'>
                     Dashboard
                 </a>
-                <a href='/agency-profile' class='mx-2'>
+                <a href='/agency-profile' className='mx-2'>
                     Agency Profile
                 </a>
-                <a href='/admin-package-add' class='mx-2'>
+                <a href='/admin-package-add' className='mx-2'>
                     Add Package
                 </a>
-                <a href='/order-package' class='mx-2'>
+                <a href='/order-package' className='mx-2'>
                     Order Package
                 </a>
             </nav>
